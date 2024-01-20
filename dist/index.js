575f28e82851ef460c74153698b7cbd29b0ee7c0
Velayutham7028 <2001722037028@mcc.edu.in>
2024-01-20 10:03:29 +0530

    Second commit

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,13 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Welcome", {
+  enumerable: true,
+  get: function get() {
+    return _Welcome.default;
+  }
+});
+var _Welcome = _interopRequireDefault(require("./components/Welcome"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
